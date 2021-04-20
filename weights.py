4a278871d9f8369bdfb14508bb4bd738b122c124
Harshita Gupta <hrsht1998@gmail.com>
2021-04-20 14:37:50 +0530

    Create weights.py

---
+++ weights.py
@@ -0,0 +1,4 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+def download_weights():
+    gdd.download_file_from_google_drive(file_id="1--7p9rRJy7WU4OmomkzM8i0veetZctTT",
+                                        dest_path ="weights/modeldense1.h4" )
